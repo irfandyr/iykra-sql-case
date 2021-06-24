31fd51cd758b0a76c2c292c0a158dea9a6f6f65e
irfandyr <86398905+irfandyr@users.noreply.github.com>
2021-06-24 14:45:59 +0700

    Add files via upload

---
+++ Irfandy Rayhan Caesario - SQL.sql
@@ -0,0 +1,66 @@
+-- NOMOR 1
+--select title, description 
+--from film f
+--where description like '%Astronaut%'
+
+-- NOMOR 2
+--select count(*)
+--from film f 
+--where rating = 'R'
+--and replacement_cost between 5 and 15
+
+-- NOMOR 3
+--select
+--	p.staff_id,
+--	s.first_name,
+--	s.last_name,
+--	count(p.payment_id) as num_of_transactions,
+--    sum(p.amount)
+--from payment p
+--join staff s 
+--on p.staff_id = s.staff_id 
+--group by 1, 2, 3
+--order by 4 desc, 5 desc
+
+-- NOMOR 4
+--select
+--	rating,
+--	round(avg(replacement_cost),2) as avg_replacement_cost 
+--from film f
+--group by rating
+
+-- NOMOR 5
+--select
+--	c.first_name,
+--	c.last_name,
+--	c.email,
+--	sum(p.amount) as total_transaction
+--from customer c
+--join payment p 
+--on c.customer_id = p.customer_id
+--group by c.customer_id
+--order by 4 desc
+--limit 5
+
+-- NOMOR 6
+--select
+--	f.title,
+--	i.store_id,
+--	count(i.film_id)
+--from inventory i
+--join film f 
+--on i.film_id = f.film_id 
+--group by 1, 2
+--order by 1
+
+-- NOMOR 7
+--select
+--	c.first_name,
+--	c.last_name,
+--	c.email,
+--	count(p.payment_id) as num_of_transactions
+--from customer c
+--join payment p
+--on c.customer_id = p.customer_id
+--group by c.customer_id
+--having count(p.payment_id) > 40
